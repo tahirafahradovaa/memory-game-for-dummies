70e2693a712d98edabc16e2b647fe1e83f316da6
tahirafahradovaa <tairafahrad@icloud.com>
2023-01-26 21:33:56 +0400

    commit

--- src/Memory.jsx
+++ src/Memory.jsx
@@ -1,21 +1,83 @@
-const TILE_COLORS = ["red", "green", "blue", "yellow"];
+import { useEffect, useState } from "react";
+
+const TILE_COLORS = [
+  "Red",
+  "Green",
+  "Blue",
+  "Yellow",
+  "red",
+  "green",
+  "blue",
+  "yellow",
+];
 
 export default function Memory() {
+  const [colorArr, setColorArr] = useState([]);
+  const [tiles, setTiles] = useState([]);
+  const [counter, setCounter] = useState(1);
+  const [selected, setSelected] = useState([]);
+  useEffect(() => {
+    setColorArr(TILE_COLORS);
+    shuffle(TILE_COLORS);
+  }, []);
   // Write your code here.
-  function handleColor() {}
+  function handleColor(item) {
+    setTiles([...tiles, item]);
+    setCounter((counter) => counter + 1);
+    if (counter === 1) {
+      if (tiles[0]?.toLowerCase() == item?.toLowerCase()) {
+        setSelected([...selected, tiles[0], item]);
+      }
+      setTimeout(() => {
+        setTiles([]);
+        setCounter(0);
+      }, [700]);
+    }
+  }
   return (
     <>
-      <h1>Memory</h1>
-      <div class="board">
-        <div class="tile" onClick={handleColor}></div>
-        <div class="tile" onClick={handleColor}></div>
-        <div class="tile" onClick={handleColor}></div>
-        <div class="tile" onClick={handleColor}></div>
-        <div class="tile" onClick={handleColor}></div>
-        <div class="tile" onClick={handleColor}></div>
-        <div class="tile" onClick={handleColor}></div>
-        <div class="tile" onClick={handleColor}></div>
+      <h1>Memory GAME</h1>
+      <div className="board">
+        {colorArr &&
+          colorArr.map((panel) => {
+            return (
+              <>
+                <div
+                  style={{
+                    background: tiles.includes(panel)
+                      ? panel
+                      : selected.includes(panel)
+                      ? panel
+                      : "white",
+                  }}
+                  className={`tile ${
+                    tiles.includes(panel)
+                      ? panel
+                      : selected.includes(panel)
+                      ? panel
+                      : ""
+                  }`}
+                  onClick={(e) => handleColor(panel)}
+                ></div>
+              </>
+            );
+          })}
       </div>
+      {selected.length === TILE_COLORS.length && (
+        <>
+          <h1>YOU WIN</h1>
+
+          <button
+            onClick={() => {
+              setSelected([]);
+              shuffle(colorArr);
+              setColorArr(TILE_COLORS);
+            }}
+          >
+            Restart the game
+          </button>
+        </>
+      )}
     </>
   );
 }
